f73dc41bacff85d75f749aa1ce48126b527798ad
kiran <kiranfuse52@gmail.com>
2023-04-03 12:45:38 +0530

    Internet Checker and Image picker

--- app/src/main/java/com/example/helper/MainActivity.java
+++ app/src/main/java/com/example/helper/MainActivity.java
@@ -5,12 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.FileNotFoundException;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
@@ -19,34 +26,60 @@ public class MainActivity extends AppCompatActivity {
     Helper helper;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
 
 
+        boolean status = helper.isConnected(MainActivity.this);
 
+        if (status == true) {
+            Toast.makeText(MainActivity.this, "Connected", Toast.LENGTH_SHORT).show();
+        } else {
 
-                boolean status=helper.isConnected(MainActivity.this);
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("please check your connection");
+            builder.create();
+            builder.setCancelable(false);
+            builder.show();
 
-                if(status==true){
-                    Toast.makeText(MainActivity.this, "Connected", Toast.LENGTH_SHORT).show();
-                }else{
-
-                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                    builder.setMessage("please check your connection");
-                    builder.create();
-                    builder .setCancelable(false);
-                    builder.show();
-
-                    }
-
-                }
-
-
-
-
-
+        }
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 0);
             }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            Uri targetUri = data.getData();
+//            uri is content provider
+
+            Bitmap bitmap;
+            //get each pixel value of image...
+
+            try {
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
+                imageView.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
+
+}
 
